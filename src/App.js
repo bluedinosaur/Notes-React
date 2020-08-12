a48cceaa288bde15f6cd8fc7303558f1aca53b6b
bluedinosaur <justin.lee26@gmail.com>
2020-08-13 01:12:43 +1000

    Added delete function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { notes } from "./notes";
-import CardList from "./components/CardList";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavigationBar } from "./components/navbar";
@@ -12,20 +11,20 @@ class App extends Component {
     this.state = {
       notes: notes,
       searchfield: "",
-      // note: {},
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeNote = this.removeNote.bind(this);
   }
 
   handleSubmit(event) {
-    // const { notes } = this.state;
     event.preventDefault();
     const { notes } = this.state,
       name = this.refs.name.value,
       category = this.refs.category.value,
       example = this.refs.example.value,
       description = this.refs.description.value;
+
     this.setState(
       {
         notes: [
@@ -38,6 +37,7 @@ class App extends Component {
           },
         ],
       },
+
       () => {
         this.refs.name.value = "";
         this.refs.category.value = "";
@@ -45,7 +45,6 @@ class App extends Component {
         this.refs.description.value = "";
       }
     );
-    console.log(notes);
   }
 
   onSearchChange = (event) => {
@@ -53,12 +52,11 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
 
-  removeNote = (event) => {
-    var array = [...this.state.people];
-    var index = array.indexOf(event.target.value);
+  removeNote = (index) => {
+    const array = [...this.state.notes];
     if (index !== -1) {
       array.splice(index, 1);
-      this.setState({ people: array });
+      this.setState({ notes: array });
     }
   };
 
@@ -93,7 +91,7 @@ class App extends Component {
                 type="text"
                 defaultValue="select category"
               >
-                <option value="" disabled selected>
+                <option value="" disabled defaultValue>
                   Select category
                 </option>
                 <option value="HTML">HTML</option>
@@ -134,7 +132,41 @@ class App extends Component {
           </form>
         </div>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList notes={filteredNotes} />
+        <div>
+          {notes.map((note, i) => {
+            return (
+              <article className="center mw7 mw-7-ns br3 hidden ba b--black-10 mv4">
+                <div>
+                  <h1 className="f3 bg-near-white br3 br--top black-60 mv0 pv2 ph3">
+                    {notes[i].name}
+                  </h1>
+                  <h2 className="f4 bg-near-white br3 br--top black-60 mv0 pv1 ph3">
+                    {notes[i].category}
+                  </h2>
+                  <div className="pa1 bt b--black-10">
+                    <p className="ph3 f6 f5-ns lh-copy measure light-red mt3">
+                      <code>{notes[i].example}</code>
+                    </p>
+                  </div>
+                  <div className="pa1 bt b--black-10">
+                    <p className="ph3 f6 f5-ns lh-copy measure mt3">
+                      {notes[i].description}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => this.removeNote(i)}
+                    className="f6 link dim br3 ba ph3 pv2 mb2 dib black"
+                  >
+                    Delete
+                  </button>
+                  <button className="f6 link dim br3 ba ph3 pv2 mb2 dib black">
+                    Edit
+                  </button>
+                </div>
+              </article>
+            );
+          })}
+        </div>
       </div>
     );
   }
